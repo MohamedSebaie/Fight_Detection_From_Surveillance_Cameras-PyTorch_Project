ff089352d82d019b39b5a9a68581d7081871821e
Mohamed Sebaie <mohamedsebaie1@gmail.com>
2021-12-16 16:36:21 +0200

    updated_utils

--- utils.py
+++ utils.py
@@ -1,19 +1,36 @@
-import torch
-import torchvision
+# For Youtube Download.
+import os
+import io 
+from pytube import YouTube
+from IPython.display import HTML
+from base64 import b64encode
+
+
+import os
 import cv2
-from google.colab.patches import cv2_imshow
-import argparse
 import time
+import copy
+import glob
+import torch
+import argparse
+import statistics
+import torchvision
 import numpy as np
+import pandas as pd
+import torch.nn as nn
+from moviepy.editor import *
+%matplotlib inline
 import albumentations as A
+from collections import deque
+from google.colab.patches import cv2_imshow
 
 # Define the transforms
 def transform_():
-	transform = A.Compose(
+    transform = A.Compose(
     [A.Resize(128, 171, always_apply=True),A.CenterCrop(112, 112, always_apply=True),
      A.Normalize(mean = [0.43216, 0.394666, 0.37645],std = [0.22803, 0.22145, 0.216989], always_apply=True)]
      )
-	return transform
+    return transform
 
 
 def frames_extraction(video_path,SEQUENCE_LENGTH):
@@ -65,3 +82,228 @@ def frames_extraction(video_path,SEQUENCE_LENGTH):
 
     # Return the frames list.
     return frames_list
+
+
+# Function To Train the Model From Pytorch Documentation
+def train_model(model, dataloaders, criterion, optimizer, num_epochs=25, is_inception=False):
+    since = time.time()
+
+    val_acc_history = []
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    # Get model outputs and calculate loss
+                    # Special case for inception because in training it has an auxiliary output. In train
+                    #   mode we calculate the loss by summing the final output and the auxiliary output
+                    #   but in testing we only consider the final output.
+                    if is_inception and phase == 'train':
+                        # From https://discuss.pytorch.org/t/how-to-optimize-inception-model-with-auxiliary-classifiers/7958
+                        outputs, aux_outputs = model(inputs)
+                        loss1 = criterion(outputs, labels)
+                        loss2 = criterion(aux_outputs, labels)
+                        loss = loss1 + 0.4*loss2
+                    else:
+                        outputs = model(inputs)
+                        loss = criterion(outputs, labels)
+
+                    _, preds = torch.max(outputs, 1)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / len(dataloaders[phase].dataset)
+            epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+            if phase == 'val':
+                val_acc_history.append(epoch_acc)
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model, val_acc_history
+
+
+
+def PredTopKClass(k,clips,model):
+  with torch.no_grad(): # we do not want to backprop any gradients
+
+      input_frames = np.array(clips)
+      
+      # add an extra dimension        
+      input_frames = np.expand_dims(input_frames, axis=0)
+
+      # transpose to get [1, 3, num_clips, height, width]
+      input_frames = np.transpose(input_frames, (0, 4, 1, 2, 3))
+
+      # convert the frames to tensor
+      input_frames = torch.tensor(input_frames, dtype=torch.float32)
+      input_frames = input_frames.to(device)
+
+      # forward pass to get the predictions
+      outputs = model(input_frames)
+
+      # get the prediction index
+      soft_max = torch.nn.Softmax(dim=1)  
+      probs = soft_max(outputs.data) 
+      prob, indices = torch.topk(probs, k)
+
+  Top_k = indices[0]
+  Classes_nameTop_k=[CLASSES_LIST[item].strip() for item in Top_k]
+  ProbTop_k=prob[0].tolist()
+  ProbTop_k = [round(elem, 5) for elem in ProbTop_k]
+  return Classes_nameTop_k[0]     #list(zip(Classes_nameTop_k,ProbTop_k))
+
+
+def PredTopKProb(k,clips,model):
+  with torch.no_grad(): # we do not want to backprop any gradients
+
+      input_frames = np.array(clips)
+      
+      # add an extra dimension        
+      input_frames = np.expand_dims(input_frames, axis=0)
+
+      # transpose to get [1, 3, num_clips, height, width]
+      input_frames = np.transpose(input_frames, (0, 4, 1, 2, 3))
+
+      # convert the frames to tensor
+      input_frames = torch.tensor(input_frames, dtype=torch.float32)
+      input_frames = input_frames.to(device)
+
+      # forward pass to get the predictions
+      outputs = model(input_frames)
+
+      # get the prediction index
+      soft_max = torch.nn.Softmax(dim=1)  
+      probs = soft_max(outputs.data) 
+      prob, indices = torch.topk(probs, k)
+
+  Top_k = indices[0]
+  Classes_nameTop_k=[CLASSES_LIST[item].strip() for item in Top_k]
+  ProbTop_k=prob[0].tolist()
+  ProbTop_k = [round(elem, 5) for elem in ProbTop_k]
+  return list(zip(Classes_nameTop_k,ProbTop_k))
+
+def downloadYouTube(videourl, path):
+
+    yt = YouTube(videourl)
+    yt = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+    if not os.path.exists(path):
+        os.makedirs(path)
+    yt.download(path)
+
+def show_video(file_name, width=640):
+  # show resulting deepsort video
+  mp4 = open(file_name,'rb').read()
+  data_url = "data:video/mp4;base64," + b64encode(mp4).decode()
+  return HTML("""
+  <video width="{0}" controls>
+        <source src="{1}" type="video/mp4">
+  </video>
+  """.format(width, data_url))
+
+
+
+
+
+def predict_on_video(video_file_path, output_file_path, SEQUENCE_LENGTH=64):
+    '''
+    This function will perform action recognition on a video using the LRCN model.
+    Args:
+    video_file_path:  The path of the video stored in the disk on which the action recognition is to be performed.
+    output_file_path: The path where the ouput video with the predicted action being performed overlayed will be stored.
+    SEQUENCE_LENGTH:  The fixed number of frames of a video that can be passed to the model as one sequence.
+    '''
+
+    # Initialize the VideoCapture object to read from the video file.
+    video_reader = cv2.VideoCapture(video_file_path)
+
+    # Get the width and height of the video.
+    original_video_width = int(video_reader.get(cv2.CAP_PROP_FRAME_WIDTH))
+    original_video_height = int(video_reader.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    # Initialize the VideoWriter Object to store the output video in the disk.
+    video_writer = cv2.VideoWriter(output_file_path, cv2.VideoWriter_fourcc('M', 'P', '4', 'V'), 
+                                   video_reader.get(cv2.CAP_PROP_FPS), (original_video_width, original_video_height))
+
+    # Declare a queue to store video frames.
+    frames_queue = deque(maxlen = SEQUENCE_LENGTH)
+    transform= transform_()
+    # Initialize a variable to store the predicted action being performed in the video.
+    predicted_class_name = ''
+
+    # Iterate until the video is accessed successfully.
+    while video_reader.isOpened():
+
+        # Read the frame.
+        ok, frame = video_reader.read() 
+        
+        # Check if frame is not read properly then break the loop.
+        if not ok:
+            break
+
+        image = frame.copy()
+        framee = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        framee = transform(image=framee)['image']
+        
+        # Appending the pre-processed frame into the frames list.
+        frames_queue.append(framee)
+
+        # Check if the number of frames in the queue are equal to the fixed sequence length.
+        if len(frames_queue) == SEQUENCE_LENGTH:
+          predicted_class_name= PredTopKClass(1,frames_queue,model_ft)
+    
+        # Write predicted class name on top of the frame.
+        if predicted_class_name=="fight":
+          cv2.putText(frame, predicted_class_name, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+        else:
+          cv2.putText(frame, predicted_class_name, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+        # Write The frame into the disk using the VideoWriter Object.
+        video_writer.write(frame)
+        
+    # Release the VideoCapture and VideoWriter objects.
+    video_reader.release()
+    video_writer.release()
