51724a99f480e9acd4563971f273add38c39f881
Mohamed Sebaie <mohamedsebaie1@gmail.com>
2021-12-21 23:31:27 +0200

    Update infer.py

--- infer.py
+++ infer.py
@@ -8,13 +8,13 @@ torch.backends.cudnn.benchmark = True
 
 parser = argparse.ArgumentParser(description='PyTorch STAM Kinetics Inference')
 parser.add_argument('--modelPath')
+parser.add_argument('--streaming', type=bool, default=False)
 parser.add_argument('--inputPath')
 parser.add_argument('--outputPath')
-parser.add_argument('--streamingPath', type=str, default="")
 parser.add_argument('--sequenceLength', type=int, default=16)
 parser.add_argument('--skip', type=int, default=2)
 parser.add_argument('--showInfo', type=bool, default=False)
-parser.add_argument('--streaming', type=bool, default=False)
+
 
 
 
@@ -26,7 +26,7 @@ def main():
     model = loadModel(args.modelPath)
     # Perform Fight Detection on the Test Video.
     if args.streaming==True:
-        start_streaming(model,args.streamingPath)
+        start_streaming(model,args.inputPath)
 
     else:
         start=time.time()
